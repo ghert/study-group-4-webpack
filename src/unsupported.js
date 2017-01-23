f7bd96084848538cb0788a32054ec68cebad1433
Wojciech Panek <wpanek@apptension.com>
2017-01-23 13:09:52 +0100

    Configure entry points and use CommonsChunkPlugin

--- src/unsupported.js
+++ src/unsupported.js
@@ -1,4 +1,4 @@
-import {documentWrite} from 'modules/common';
-import copy from 'copy';
+import {documentWrite} from './modules/common';
+import copy from './copy';
 
 documentWrite(`<h1>${copy.unsupported}</h1>`);
--- webpack-base.js
+++ webpack-base.js
@@ -1,12 +1,26 @@
 'use strict';
 
+import CommonsChunkPlugin from 'webpack/lib/optimize/CommonsChunkPlugin';
+
 export default {
-    entry: {},
-    output: {},
+    entry: {
+        main: "./src/main.js",
+        unsupported: "./src/unsupported.js",
+    },
+    output: {
+        path: "./dist",
+        filename: "[name].js"
+    },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.json/, loader: 'json-loader'}
         ]
     },
-    plugins: []
+    plugins: [
+        new CommonsChunkPlugin({
+            name: "commons",
+            filename: "commons.js",
+        })
+    ]
 };
