c49dea85968cd708898206e303794462a1e5615c
Wojciech Panek <wpanek@apptension.com>
2017-01-25 11:28:21 +0100

    Configure sass loader

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {documentWrite} from './modules/common';
 import copy from './copy';
 import {log} from './modules/log';
 import {clamp} from './modules/math';
+import './styles/main.scss';
 // import config from 'config';
 
 documentWrite(`<h1>${copy.main} []</h1><p>It works!</p>`);
--- webpack-base.js
+++ webpack-base.js
@@ -1,26 +1,36 @@
 'use strict';
 
 import CommonsChunkPlugin from 'webpack/lib/optimize/CommonsChunkPlugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const extractCSS = new ExtractTextPlugin('[name]-[hash].css');
 
 export default {
     entry: {
         main: "./src/main.js",
-        unsupported: "./src/unsupported.js",
+        unsupported: "./src/unsupported.js"
     },
     output: {
         path: "./dist",
-        filename: "[name].js"
+        filename: "[name]-[hash].js"
     },
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.json/, loader: 'json-loader'}
+            { test: /\.json/, loader: 'json-loader'},
+            { test: /\.scss$/i, loader: extractCSS.extract(['css-loader','sass-loader']) },
+            {
+                test: /\.(png|jpg|gif|ico|woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file',
+                query: { name: '[name]-[hash].[ext]' }
+            }
         ]
     },
     plugins: [
         new CommonsChunkPlugin({
             name: "commons",
-            filename: "commons.js",
-        })
+            filename: "commons-[hash].js"
+        }),
+        extractCSS
     ]
 };
