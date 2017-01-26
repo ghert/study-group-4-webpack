e681f6a8b0539a7c8bee7d5f48736310a2666d55
Wojciech Panek <wpanek@apptension.com>
2017-01-26 09:25:06 +0100

    Add envConfig support

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import copy from './copy';
 import {log} from './modules/log';
 import {clamp} from './modules/math';
 import './styles/main.scss';
-// import config from 'config';
+import config from 'config';
 
-documentWrite(`<h1>${copy.main} [] <i></i></h1><p>It works!</p>`);
+documentWrite(`<h1>${copy.main} [${config.name}] <i></i></h1><p>It works!</p>`);
 
 log('Clamped value: ' + clamp(Math.random(), .25, .75));
--- webpack-base.js
+++ webpack-base.js
@@ -9,61 +9,67 @@ import path from 'path';
 
 const extractCSS = new ExtractTextPlugin('[name]-[hash].css');
 
+const envMap = {
+  dev: 'local',
+  prod: 'prod'
+};
+
 export default {
-    entry: {
-        main: './src/main.js',
-        unsupported: './src/unsupported.js'
-    },
-    output: {
-        path: './dist',
-        filename: '[name]-[hash].js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.json/, loader: 'json-loader'},
-            { test: /\.scss$/i, loader: extractCSS.extract(['css-loader','sass-loader', 'postcss-loader']) },
-            {
-                test: /\.(png|jpg|gif|ico|woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file',
-                query: { name: '[name]-[hash].[ext]' }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            images: path.resolve( __dirname, 'images' )
-        }
-    },
-    plugins: [
-        new CommonsChunkPlugin({
-            name: 'commons',
-            filename: 'commons-[hash].js'
-        }),
-        new HtmlWebpackPlugin(),
-        new SpritesmithPlugin({
-            retina: '@2x',
-            src: {
-                cwd: './images/sprites',
-                glob: '*.png'
-            },
-            target: {
-                image: './images/generated/sprite.png',
-                css: './src/generated/styles/sprites.scss'
-            },
-            apiOptions: {
-                cssImageRef: `~images/generated/sprite.png`
-            },
-            spritesmithOptions: {
-                algorithm: 'top-down',
-                padding: 1
-            }
-        }),
-        extractCSS
-    ],
-    postcss: () => {
-        return [
-            autoprefixer
-        ];
+  entry: {
+    main: './src/main.js',
+    unsupported: './src/unsupported.js'
+  },
+  output: {
+    path: './dist',
+    filename: '[name]-[hash].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel', exclude: /node_modules/},
+      {test: /\.json/, loader: 'json-loader'},
+      {test: /\.scss$/i, loader: extractCSS.extract(['css-loader', 'sass-loader', 'postcss-loader'])},
+      {
+        test: /\.(png|jpg|gif|ico|woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file',
+        query: {name: '[name]-[hash].[ext]'}
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      images: path.resolve(__dirname, 'images'),
+      config: path.resolve(__dirname, 'src', 'env', envMap[process.env.NODE_ENV], 'conf')
     }
+  },
+  plugins: [
+    new CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons-[hash].js'
+    }),
+    new HtmlWebpackPlugin(),
+    new SpritesmithPlugin({
+      retina: '@2x',
+      src: {
+        cwd: './images/sprites',
+        glob: '*.png'
+      },
+      target: {
+        image: './images/generated/sprite.png',
+        css: './src/generated/styles/sprites.scss'
+      },
+      apiOptions: {
+        cssImageRef: `~images/generated/sprite.png`
+      },
+      spritesmithOptions: {
+        algorithm: 'top-down',
+        padding: 1
+      }
+    }),
+    extractCSS
+  ],
+  postcss: () => {
+    return [
+      autoprefixer
+    ];
+  }
 };
